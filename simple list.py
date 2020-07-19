83853ae852a440c36bcf1544dc5192f0fbf31da3
codewyf <1090187390@qq.com>
2020-07-19 14:14:45 +0800

    update simple list

--- simple list.py
+++ simple list.py
@@ -36,4 +36,16 @@ mix[0] = mix[-1]
 mix[-1]
 
 import random
-print('随机抽取',random.choice(mix))
+print('随机抽取:',random.choice(mix))
+
+mix.remove("codewyf")#使用remove（）删除元素，并不知道这个元素在列表中的具体位置。但是如果制定的元素不存在与列表中，会报错
+print(mix)
+
+mix.pop(0)#pop需要一个索引值，取出并删除该元素
+print(mix)
+
+mix.pop()#如果不带参数，默认弹出列表最后一个元素
+print(mix)
+
+del mix[0]
+print(mix)

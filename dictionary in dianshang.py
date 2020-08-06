ce7be8327fe93fb8149d10bf622fcf0185e49599
codewyf <1090187390@qq.com>
2020-08-06 17:03:32 +0800

    push dictionary in dianshang & find unique price using list

--- dictionary in dianshang.py
+++ dictionary in dianshang.py
@@ -1,24 +1,21 @@
-# -*- coding: utf-8 -*-
 """
-@Time ： 2020/7/31 5:03 下午
+@Time ： 2020/8/6 16:30 下午
 @Auth ： Codewyf
 @File ：list in dianshang.py
 @IDE ：PyCharm
 @Motto：Go Ahead Instead of Heasitating
 
 """
+
 def find_product_price(products, product_id):
     for id, price in products:
         if id == product_id:
             return price
     return None
 
-#list
-products = [
-    (143121312, 100),
-    (432314553, 30),
-    (32421912367, 150)
-]
-
-print('The price of product 432314553 is {}'.format(find_product_price(products, 432314553)))
-
+products = {
+    143121312: 100,
+    432314553: 30,
+    32421912367: 150
+}
+print('The price of product 432314553 is {}'.format(products[432314553]))
---
+++ find unquie price using list.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@Time:2020/8/6 16:42
+@Auth:Codewyf
+@File:find unquie price using list.py
+@IDE:PyCharm
+@Motto: Go Ahead Instead Of Heasitating
+"""
+#如果还是选择使用列表，对应的代码如下，其中，A 和 B 是两层循环。同样假设原始列表有 n 个元素，那么，在最差情况下，需要 O(n^2) 的时间复杂度。
+#list version
+def find_unique_price_using_list(products):
+    unique_price_list = []
+    for _, price in products: #A
+        if price not in unique_price_list: #B
+            unique_price_list.append(price)
+    return len(unique_price_list)
+
+products = [
+    (143121312, 100),
+    (432314553, 30),
+    (32421912367, 150),
+    (937153201, 30)
+             ]
+
+print('number of unique price is :{}'.format(find_unique_price_using_list(products)))
+

85e305bf5a39ee13668b9858b33bf65e34c1e4b2
codewyf <1090187390@qq.com>
2021-06-26 18:14:29 +0100

    use fashion_mnist dataset

--- pytorch_learning_Limu/minst_classification.py
+++ pytorch_learning_Limu/minst_classification.py
@@ -7,3 +7,24 @@
 @Motto：Go Ahead Instead of Hesitating
 
 """
+
+
+# Fashion_MNIST
+
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+
+d2l.use_svg_display()
+
+trans = transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(
+    root="../data", train=True,
+    transform=trans, download=True
+)
+mnist_test = torchvision.datasets.FashionMNIST(
+    root="../data", train=False,
+    transform=trans, download=True
+)

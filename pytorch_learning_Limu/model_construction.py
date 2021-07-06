8811d597f15ef66c64fc01d4daa75f1ca0882d13
codewyf <1090187390@qq.com>
2021-07-06 06:06:54 +0100

    lecture 12

---
+++ pytorch_learning_Limu/model_construction.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2021/7/4 1:21 am
+@Auth ： Codewyf
+@File ：model_construction.py
+@IDE ：PyCharm
+@Motto：Go Ahead Instead of Hesitating
+
+"""
--- pytorch_learning_Limu/weight_decay.py
+++ pytorch_learning_Limu/weight_decay.py
@@ -74,3 +74,5 @@ def train_concise(wd):
 
 train_concise(0)
 train_concise(3)
+
+

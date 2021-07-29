28daaedfc2a06c8cf8b6f31503f0674b3e86e5e5
codewyf <1090187390@qq.com>
2021-07-29 23:43:22 +0100

    lecture 16 FileIO

--- pytorch_learning_Limu/FileIO.py
+++ pytorch_learning_Limu/FileIO.py
@@ -23,3 +23,33 @@ y = torch.zeros(4)
 torch.save([x, y], 'x-files')
 x2, y2 = torch.load('x-files')
 print(f'x2:{x2}, y2:{y2}')
+
+mydict = {'x': x, 'y': y}
+torch.save(mydict, 'mydict')
+mydict2 = torch.load('mydict')
+print(f'mydict2: {mydict2}')
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden = nn.Linear(20, 256)
+        self.output = nn.Linear(256, 10)
+
+    def forward(self, x):
+        return self.output(F.relu(self.hidden(x)))
+
+
+net = MLP()
+X = torch.randn(size=(2, 20))
+Y = net(X)
+# save parameters
+torch.save(net.state_dict(), 'mlp.params')
+
+clone = MLP()
+clone.load_state_dict(torch.load('mlp.params'))
+clone.eval()
+print(f'clone: {clone}')
+
+Y_clone = clone(X)
+print(Y_clone == Y)

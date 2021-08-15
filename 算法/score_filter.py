afb5f7ac6689780892130cfd50c07c9f2cb27cbe
codewyf <1090187390@qq.com>
2021-08-15 23:29:40 +0100

    洪朴面试编程大题

--- 算法/score_filter.py
+++ 算法/score_filter.py
@@ -7,3 +7,28 @@
 @Motto：Go Ahead Instead of Hesitating
 
 """
+from operator import itemgetter
+predictions = [['A', 'B', 'A'], [(1,1,4,4), (3,3,6,6), (0,1,8,7)], [0.98, 0.84, 0.85]]
+
+threshold = {'A': 0.95, 'B': 0.90}
+
+
+def filter(predictions, threshold):
+    for i in range(len(predictions[0])):
+        if predictions[0][i] == 'A':
+            if predictions[2][i] > threshold['A']:
+                result = [x[i] for x in predictions ]
+                print(f'filtered_predictions = {result}')
+            else:
+                print('The score is lower than threshold')
+        else:
+            if predictions[0][i] == 'B':
+                if predictions[2][i] > threshold['B']:
+                    result = [x[i] for x in predictions]
+                    print(f'filtered_predictions = {result}')
+                else:
+                    print('The score is lower than threshold')
+
+
+
+filter(predictions, threshold)
